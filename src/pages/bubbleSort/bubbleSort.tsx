32f822f939154452ff2651b57d17463d2b8c613c
Jose Padilla <jose.apadilla99@gmail.com>
2023-09-02 14:56:53 -0500

    feat: items are in state

--- src/pages/bubbleSort/bubbleSort.tsx
+++ src/pages/bubbleSort/bubbleSort.tsx
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function BubbleSort() {
-  function randomizer(max: number, amount: number) {
-    for(let i = 0; i < amount; i++) {
-        const num = Math.floor(Math.random() * max);
-        items.push(num)
-    }
-}
-let items:Array<number> = []
+  let [items, setItems] = useState(randomizer(100, 5));
 
-randomizer(100, 5)
-  console.log(items)
-  return(
-      <div id="bubbleSort_parent_container">
-        
-      </div>
-  ) 
+  function randomizer(max: number, amount: number) {
+    const randomNums: Array<number> = [];
+    for (let i = 0; i < amount; i++) {
+      const num = Math.floor(Math.random() * max);
+      randomNums.push(num);
+    }
+    return randomNums;
+  }
+
+  const randomNums = items.map((number, index) => {
+    return <p key={index}>{number}</p>;
+  });
+
+  return (
+    <div id="bubbleSort_parent_container">
+      <div>{randomNums}</div>
+    </div>
+  );
 }
