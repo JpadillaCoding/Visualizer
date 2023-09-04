a1e5c5e423e23656520cbb084114322bfb726c1b
Jose Padilla <jose.apadilla99@gmail.com>
2023-09-04 09:48:45 -0500

    feat: added optimization to end algo early if already sorted

--- src/pages/bubbleSort/bubbleSort.tsx
+++ src/pages/bubbleSort/bubbleSort.tsx
@@ -11,22 +11,24 @@ export default function BubbleSort() {
     }
     return randomNums;
   }
-
+  console.log(items)
   function bubbleAlgo(arr: Array<number>, len: number) {
-    for (let i = 0; i < len - 1; i++) {
-      for (let j = 0; j < len - i - 1; j++) {
+    let swapped: boolean = false; // check if there have been any swaps
+    for (let i = 0; i < len; i++) { //outer loop to go through each index
+      swapped = false;
+      for (let j = 0; j < len; j++) { //inner loop used for comparison and moving items to sort
         if (arr[j] > arr[j + 1]) {
           let temp = arr[j];
           arr[j] = arr[j + 1];
           arr[j + 1] = temp;
+          swapped = true;
           setItems([...arr]);
         }
       }
+      console.log(items)
+      if (!swapped) break;
     }
   }
-  useEffect(() => {
-    console.log(items);
-  }, [items]);
 
   const renderedNums = items.map((number, index) => {
     return <p key={index}>{number}</p>;
