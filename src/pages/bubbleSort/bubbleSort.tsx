6e397329e5eef514e0f1c5e8d0e064fb9ae7a0fe
Jose Padilla <jose.apadilla99@gmail.com>
2023-09-04 12:08:33 -0500

    feat: set a delay in the algo for visual assistance

--- src/pages/bubbleSort/bubbleSort.tsx
+++ src/pages/bubbleSort/bubbleSort.tsx
@@ -4,20 +4,23 @@ import randomizer from "../../helpers/randomzier";
 export default function BubbleSort() {
   let [items, setItems] = useState(randomizer(100, 5));
 
-  function bubbleAlgo(arr: Array<number>, len: number) {
+  async function bubbleAlgo(arr: Array<number>, len: number) {
     let swapped: boolean = false; // check if there have been any swaps
-    for (let i = 0; i < len; i++) { //outer loop to go through each index
+    for (let i = 0; i < len; i++) {
+      //outer loop to go through each index
       swapped = false;
-      for (let j = 0; j < len; j++) { //inner loop used for comparison and moving items to sort
+      for (let j = 0; j < len; j++) {
+        //inner loop used for comparison and moving items to sort
         if (arr[j] > arr[j + 1]) {
           let temp = arr[j];
           arr[j] = arr[j + 1];
           arr[j + 1] = temp;
           swapped = true;
-          setItems([...arr]);
+          await new Promise((resolve) => setTimeout(resolve, 3000));
+          setItems([...arr])
         }
       }
-      console.log(items)
+      console.log(items);
       if (!swapped) break;
     }
   }
@@ -25,7 +28,7 @@ export default function BubbleSort() {
   const renderedNums = items.map((number, index) => {
     return <p key={index}>{number}</p>;
   });
-  
+
   return (
     <div id="bubbleSort_parent_container">
       <div>{renderedNums}</div>
